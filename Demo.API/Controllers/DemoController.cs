4379556f5f13a053243cfb014f13bcd011cb7b8f
carlos <cra.alcon@gmail.com>
2024-08-09 18:41:55 +0200

    Add project files.

---
+++ Demo.API/Controllers/DemoController.cs
@@ -0,0 +1,23 @@
+﻿using Demo.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.API.Controllers
+{
+    [ApiController]
+    [Route("/api/DemoBot")]
+    public class DemoController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        public DemoController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _dataContext.Demos.ToListAsync());
+        }
+    }
+}
---
+++ Demo.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Demo.Common.Enttities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.API.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<DemoDTO> Demos { get; set; }
+    }
+
+}
